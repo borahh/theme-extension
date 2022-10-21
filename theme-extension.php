068e7d450f16813e51a4a9ef4bbac5358e8e075d
Himanshu Borah <borahhimanshu3@gmail.com>
2022-10-21 19:29:09 +0530

    Custom Fields

--- theme-extension.php
+++ theme-extension.php
@@ -8,4 +8,11 @@
    Author URI: NA
    License: GPL2
    */
+
+
+
+/**
+ * For custom fields : http://imzbswh.cluster028.hosting.ovh.net/islandchic/wp-admin/plugin-editor.php?file=realhomes-vacation-rentals%2Fadmin%2Frvr-booking-post-type.php&plugin=realhomes-vacation-rentals%2Frealhomes-vacation-rentals.php
+**/
+
 ?>
