7b6362d7e68dde11f924f71cd0e641c1cc3426a8
Dinesh485 <ghantasaladinesh@gmail.com>
2022-10-25 15:51:52 +0530

    .

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,5 +1,8 @@
 window.addEventListener('load', () =>{
     const optionalDates = document.querySelector('#property-availability').getAttribute('data-option-dates').split(',')
-    console.log(optionalDates)
-    
+    const jsonOptionalDates = optionalDates.map(item =>{
+        const [year, month, day] = item.split('-')
+        return {year, month, day}
+    })
+    console.log(jsonOptionalDates)
 })
