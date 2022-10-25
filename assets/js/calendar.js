5b409847b8c35bbcb59311118c6b6eba6fa5755c
Dinesh485 <ghantasaladinesh@gmail.com>
2022-10-25 15:40:02 +0530

    .

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () =>{
+document.addEventListener('load', () =>{
     const optionalDates = document.querySelector('#property-availability').getAttribute('data-option-dates')
     console.log(optionalDates)
 })
