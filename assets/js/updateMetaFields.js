9f72264cfb3ada414fcfaca3dbc6f8aebbfdf158
Himanshu Borah <borahhimanshu3@gmail.com>
2022-11-04 22:59:42 +0530

    -

--- assets/js/updateMetaFields.js
+++ assets/js/updateMetaFields.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($) {
       if (!matches) return;
       const num = parseInt(matches[1]);
       if (isNaN(num)) return;
-      const target = $(`#rvr_seasonal_plans_${num}__rvr_seasonal_plans_group_${num}_rvr_plan_night_price`);
+      const target = $(`#rvr_seasonal_plans_${num}_rvr_seasonal_plans_group_${num}_rvr_plan_night_price`);
       if (!target.length) return;
       const val = parseInt(e.target.value);
       target.eq(0).val(isNaN(val) ? 0 : val*val);
