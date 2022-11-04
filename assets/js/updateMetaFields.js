8a8defc5dbe83b2b958dd3fb366eb8ff31345300
Himanshu Borah <borahhimanshu3@gmail.com>
2022-11-04 22:55:38 +0530

    Works?

--- assets/js/updateMetaFields.js
+++ assets/js/updateMetaFields.js
@@ -1,8 +1,14 @@
-
 jQuery(document).ready(function($) {
-    $('body').on('keyup', '#rvr_seasonal_plans_2_rvr_seasonal_plans_group_1_rvr_plan_week_price', function() {
-        var width = $('#rvr_seasonal_plans_2_rvr_seasonal_plans_group_1_rvr_plan_week_price').val();
-        var square_meters = width;
-        $('#rvr_seasonal_plans_2_rvr_seasonal_plans_group_1_rvr_plan_night_price').val(square_meters);
-    });
-});
+    $('body').on('keyup', function (e) {
+       const id = $(e.target).attr('id');
+      if (!id) return;
+      const matches = id.match(/rvr_seasonal_plans_(\d*)_rvr_seasonal_plans_group_(\d*)_rvr_plan_night_price/);
+      if (!matches) return;
+      const num = parseInt(matches[1]);
+      if (isNaN(num)) return;
+      const target = $(`#rvr_seasonal_plans_${num}__rvr_seasonal_plans_group_${num}_rvr_plan_night_price`);
+      if (!target.length) return;
+      const val = parseInt(e.target.value);
+      target.eq(0).val(isNaN(val) ? 0 : val*val);
+    })
+  });
