93d2135e9ccd4ba45ed2652a21201329eb2d8271
Himanshu Borah <borahhimanshu3@gmail.com>
2022-11-04 22:13:11 +0530

    xyz

--- assets/js/updateMetaFields.js
+++ assets/js/updateMetaFields.js
@@ -1,1 +1,8 @@
 
+jQuery(document).ready(function($) {
+    $('body').on('keyup', '#rvr_seasonal_plans_2_rvr_seasonal_plans_group_1_rvr_plan_week_price', function() {
+        var width = $('#rvr_seasonal_plans_2_rvr_seasonal_plans_group_1_rvr_plan_week_price').val();
+        var square_meters = width;
+        $('#rvr_seasonal_plans_2_rvr_seasonal_plans_group_1_rvr_plan_night_price').val(square_meters);
+    });
+});
