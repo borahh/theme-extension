3979679f96661c08c8f87dbf65b52db8adf5a4ae
Himanshu Borah <borahhimanshu3@gmail.com>
2022-11-04 23:25:33 +0530

    A

--- assets/js/updateMetaFields.js
+++ assets/js/updateMetaFields.js
@@ -2,11 +2,11 @@ jQuery(document).ready(function($) {
     $('body').on('keyup', function (e) {
        const id = $(e.target).attr('name');
       if (!id) return;
-      const matches = id.match(/rvr_seasonal_plans_[(\d*)]_rvr_seasonal_plans_group_[(\d*)]_rvr_plan_week_price/);
+      const matches = id.match(/rvr_seasonal_plans[(\d*)][rvr_seasonal_plans_group][(\d*)][rvr_plan_week_price]/);
       if (!matches) return;
       const num1 = parseInt(matches[1]), num2 = parseInt(matches[2]);
       if (isNaN(num1) || isNaN(num2)) return;
-      const target = $(`[name="rvr_seasonal_plans_[${num1}]_rvr_seasonal_plans_group_[${num2}]_rvr_plan_night_price"]`);
+      const target = $(`[name="rvr_seasonal_plans[${num1}][rvr_seasonal_plans_group][${num2}][rvr_plan_night_price]"]`);
       if (!target.length) return;
       const val = parseInt(e.target.value);
       target.eq(0).val(isNaN(val) ? 0 : val*val);
