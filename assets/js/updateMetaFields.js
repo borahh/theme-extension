713c94f9ee11f0c09b3a623cabc4e15034ba9cbb
Himanshu Borah <borahhimanshu3@gmail.com>
2022-11-04 23:09:34 +0530

    Works!

--- assets/js/updateMetaFields.js
+++ assets/js/updateMetaFields.js
@@ -4,9 +4,9 @@ jQuery(document).ready(function($) {
       if (!id) return;
       const matches = id.match(/rvr_seasonal_plans_(\d*)_rvr_seasonal_plans_group_(\d*)_rvr_plan_week_price/);
       if (!matches) return;
-      const num = parseInt(matches[1]);
-      if (isNaN(num)) return;
-      const target = $(`#rvr_seasonal_plans_${num}_rvr_seasonal_plans_group_${num}_rvr_plan_night_price`);
+      const num1 = parseInt(matches[1]), num2 = parseInt(matches[2]);
+      if (isNaN(num1) || isNaN(num2)) return;
+      const target = $(`#rvr_seasonal_plans_${num1}_rvr_seasonal_plans_group_${num2}_rvr_plan_night_price`);
       if (!target.length) return;
       const val = parseInt(e.target.value);
       target.eq(0).val(isNaN(val) ? 0 : val*val);
