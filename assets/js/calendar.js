e1596b2a6ca960932a828c0e0d807f572e2c0910
Dinesh485 <ghantasaladinesh@gmail.com>
2022-11-09 19:26:17 +0530

    .

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -189,5 +189,11 @@ console.log(availablePlans)
 const newCheckOut = checkOut.cloneNode(true)
 checkOut.parentNode.replaceChild(newCheckOut, checkOut)
 
+newCheckOut.daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+
 })
 
