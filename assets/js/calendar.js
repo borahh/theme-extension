6e00321cf777ddc441748665a02b134a205801f0
Dinesh485 <ghantasaladinesh@gmail.com>
2022-11-26 14:43:30 +0530

    .

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -216,14 +216,16 @@ const pageNav = `<nav id = 'page_nav' >
    <ul class = 'rh_wrap--padding'>
      ${navLinksHTML}
    </ul>
-   <svg class = "left" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+   <div class = "left">
+   <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
 </svg>
-
-<svg class = "right" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+</div>
+<div class = "right">
+<svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
 </svg>
-
+</div>
 
 </nav>`
 
