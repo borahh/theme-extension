fcd15f9d9339f9d6e34e644cd8be864c04dbdc03
Dinesh485 <ghantasaladinesh@gmail.com>
2022-12-09 14:38:06 +0530

    .

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -159,6 +159,252 @@ const markOnDatePicker = () =>{
     })
 }
 
+async function calculateCost(startDate, endDate, flag) {
+
+    // Setting the Check-In and Check-Out dates in their fields.
+    const checkIn = document.querySelector('input[nane="check_in"]')
+    const checkOut = document.querySelector('input[nane="check_out"]')
+    let pricePerNight = document.querySelector('.price-per-night').value
+    const propertyPricingType = document.querySelector('.property-pricing').value; // Seasonal / flat.
+    checkIn.value = startDate
+    checkOut.value = endDate
+    let bulkPrices;
+            try {
+                bulkPrices = JSON.parse(document.querySelector('.bulk-prices').value);
+            } catch (error) {
+                bulkPrices = {};
+            }
+   
+        // Do nothing and return if Price Per Night is not available.
+        if (0 === parseInt(pricePerNight)) {
+            return;
+        }
+
+        let days = parseInt((endDate.split('-')[2]- startDate.split('-')[2]) / 1000 / 60 / 60 / 24);
+        days = (days === 0) ? 1 : days; // Total days for booking.
+
+        // Change default per night price if bulk pricing is applicable.
+        var defaultPricePerNight = null;
+        bulkPrices.forEach((value, key) =>{
+          if (days >= parseInt(key)) {
+            defaultPricePerNight = parseInt(value);
+        }
+        })
+
+       if (null === defaultPricePerNight) {
+            defaultPricePerNight = pricePerNight
+        }
+
+
+        // Set basic cost of staying nights. Also apply seasonal pricing if applicable.
+        var costStayingNights = null;
+
+        if ('seasonal' === propertyPricingType) {
+
+            var fetchStayingNightsCost = {
+                type: 'post',
+                url: ajaxurl,
+                data: {
+                    action: 'fetch_staying_nights_cost',
+                    property_id: propertyID,
+    adult:  parseInt(document.querySelector('select.rvr-adult').val()),
+                    default_price: defaultPricePerNight,
+                    check_in: startDate.format('YYYY-MM-DD'),
+                    check_out: endDate.format('YYYY-MM-DD'),
+                },
+                success: function (response) {
+                    costStayingNights = parseInt(response);
+                }
+            };
+            await $.ajax(fetchStayingNightsCost);
+
+
+        } else {
+            costStayingNights = defaultPricePerNight * days;
+        }
+
+
+
+        // Average price per night as different per night prices are may applied.
+        var avgPricePerNight = Math.round(costStayingNights / days);
+
+        // Calculate service charges.
+        var costServiceCharges = (costStayingNights * serviceCharges) / 100;
+        costServiceCharges = (isNaN(costServiceCharges)) ? 0 : costServiceCharges;
+
+        // Guests data.
+        const book_child_as = document.querySelector('.book-child-as').value;
+        const children = document.querySelector('select.rvr-child');
+        const adults = document.querySelector('select.rvr-adult');
+        const adultsNum = parseInt(adults.val());
+        let guestsNum = 0;
+
+        if ('adult' === book_child_as) { // Check if child needs to be booked as an adult.
+            const childrenNum = parseInt(children.value);
+            guestsNum = adultsNum + childrenNum;
+        } else {
+            guestsNum = adultsNum;
+        }
+
+        const guestsCapacity = document.querySelector('.guests-capacity').value;
+        const extraGuestsNum = ((guestsNum - guestsCapacity) > 0) ? guestsNum - guestsCapacity : 0;
+        const extraGuests = document.querySelector('.extra-guests').value;
+        const perExtraGuestPrice = parseInt(document.querySelector('.per-extra-guest-price').value);
+        var costExtraGuests = 0;
+
+        // Extra guests cost calculation
+        if ('allowed' === extraGuests && Number.isInteger(perExtraGuestPrice)) {
+            costExtraGuests = perExtraGuestPrice * (extraGuestsNum * days);
+        }
+
+        // Calculate additional fees.
+        var additionalFeesFields = document.querySelector('.rvr-additional-fees');
+        var additionalFeesAmount = 0; // Total amount of all additional fees.
+        var additionalFees = []; // Array of all additional fees fields data.
+        var additionalFeesPrices = []; // Array of names as keys and prices as values for the price format and then display purpose.
+
+        // Prepare additional fees data if it's available.
+        if (additionalFeesFields) {
+            // Number of guests information.
+            adults.off('change.calculation');
+            adults.on('change.calculation', function () { // Redo the calculations on adults change.
+                calculateCost(startDate, endDate, null);
+            });
+
+            if ('adult' === book_child_as) { // Check if child will be booked as an adult.
+                children.off('change.calculation');
+                children.on('change.calculation', function () { // Redo the calculations on children change.
+                    calculateCost(startDate, endDate, null);
+                });
+            }
+
+            additionalFeesFields = additionalFeesFields.children; // Assign all additional fees fields to the fields variable if exists.
+
+            // Loop through all additional fees fields and build an array from their data.
+            additionalFeesFields.forEach(function(item) {
+
+                // Fee data gathering from its field.
+                let fee = [];
+                fee['name'] = item.getAttribute('name');
+                fee['label'] = item.dataset.label;
+                fee['type'] = item.dataset.type;
+                fee['calc'] = item.dataset.calculation;
+                fee['amount'] = parseInt(item.dataset.amount);
+
+                if ('per_stay' !== fee['calc']) {
+                    switch (fee['calc']) {
+                        case 'per_guest':
+                            fee['amount'] = fee['amount'] * guestsNum; // Per guest fee.
+                            break;
+                        case 'per_night':
+                            fee['amount'] = fee['amount'] * days; // Per night fee.
+                            break;
+                        case 'per_night_guest':
+                            fee['amount'] = fee['amount'] * (guestsNum * days); // Per guest per night fee.
+                    }
+                }
+
+                // Apply the percentage of staying nights cost if fee type is percentage.
+                if ('percentage' === fee['type']) {
+                    fee['amount'] = (costStayingNights * fee['amount']) / 100;
+                }
+
+                item.value = fee['amount'];
+                additionalFeesAmount += fee['amount']; // Add current fee to the total amount of the fees.
+                additionalFeesPrices[fee['name']] = fee['amount']; // Add to the pricing array for the price formation and display in calculation table purpose.
+
+                additionalFees.push(fee); // Push current fee data to the fees data array.
+            });
+        }
+
+        // Calculate sub total.
+        var costSubTotal = costStayingNights + costServiceCharges + additionalFeesAmount + costExtraGuests;
+
+        // Calculate Govt. taxes.
+        var costGovtTax = (costSubTotal * govtTax) / 100;
+        costGovtTax = (isNaN(costGovtTax)) ? 0 : costGovtTax;
+
+        // Prepare total cost of current booking.
+        var costTotal = costSubTotal + costGovtTax;
+
+          const data =  {
+            action: 'rvr_format_prices',
+            prices: {
+                avgPricePerNight,
+                costStayingNights,
+                costExtraGuests,
+                costServiceCharges,
+                ...additionalFeesPrices,
+                costGovtTax,
+                costSubTotal,
+                costTotal,
+            }
+        }
+        // Format prices to display in the calculation table.
+        fetch(document.querySelector('.rvr-booking-form').getAttribute('action'),
+            {
+              method: 'POST',
+                body:JSON.stringify(data),
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+            }
+        ).then(res => res.json()).then(function (response) { // Set prices with their other relevant data to the calculation table and then display the table.
+          var responseJson = response;
+          var prices = responseJson.formatted_prices; // Formatted prices.
+
+          // Set data and values for the default booking calculation fields.
+          const snField = document.querySelector('.staying-nights-count-field .cost-value');
+          const psnField = document.querySelector('.staying-nights-field .cost-value');
+          const scField = document.querySelector('.services-charges-field .cost-value');
+          const stField = document.querySelector('.subtotal-price-field .cost-value');
+          const gtField = document.querySelector('.govt-tax-field .cost-value');
+          const tpField = document.querySelector('.total-price-field .cost-value');
+
+          snField.innerText =Math.round(days) + ' x ' + prices.avgPricePerNight;
+          psnField.innerText =prices.costStayingNights;
+          scField.innerText =prices.costServiceCharges;
+          stField.innerText =prices.costSubTotal;
+          gtField.innerText =prices.costGovtTax;
+          tpField.innerText =prices.costTotal;
+
+          snField.dataset['avg-price-per-night'] =  Math.round(avgPricePerNight);
+          snField.dataset['total-nights'] =  Math.round(days);
+          psnField.dataset['staying-nights'] =  Math.round(costStayingNights);
+          scField.dataset['service-charges'] =  Math.round(costServiceCharges);
+          stField.dataset['subtotal'] =  Math.round(costSubTotal);
+          gtField.dataset['govt-tax'] =  Math.round(costGovtTax);
+          tpField.dataset['total'] =  Math.round(costTotal);
+
+          // Additional guest details display.
+          const egField = document.querySelector('.extra-guests-field');
+          if (extraGuestsNum) {
+              const egFieldVal = egField.querySelector('.cost-value');
+
+              egField.style.display = 'block';
+              egField.querySelector('span').innerText = extraGuestsNum ;
+              egFieldVal.innerText = prices.costExtraGuests;
+              egFieldVal.dataset['extra-guests'] = Math.round(costExtraGuests);
+          } else {
+              egField.style.display= 'none'
+          }
+
+          // Set additional fees fields data and values in the calculation table.
+          if (additionalFees) {
+              additionalFees.forEach(function (field) {
+                  let feeCalcField = document.querySelector('.' + field['name'] + '-fee-field').querySelector('.cost-value');
+                  feeCalcField.innerText = prices[field['name']];
+                  feeCalcField.data(prices[field['name']].match(/\d/g).join(''));
+              });
+          }
+      });
+
+
+
+    
+
+}
+
 
 window.addEventListener('load', () =>{
     const observer = new MutationObserver(markOnCalendar)
@@ -268,8 +514,7 @@ window.addEventListener('scroll', () =>{
    }
 })
 
-// // make request when page loads
-// const checkOutSelector = document.querySelector('input[name="check_out"]')
-//  checkOutSelector.data('daterangepicker').setStartDate('03/01/2025'); 
+//  make request when page loads
+
 })
 
