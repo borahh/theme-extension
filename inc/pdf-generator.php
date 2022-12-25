c28642fdef33e1ac1d302a301ad1fdb7b624cf13
Himanshu Borah <borahhimanshu3@gmail.com>
2022-12-25 19:44:04 +0530

    ===

--- inc/pdf-generator.php
+++ inc/pdf-generator.php
@@ -7,9 +7,6 @@ add_filter( 'manage_edit-booking_columns', function ( $columns ){
 
 add_action( 'manage_booking_custom_column', function ( $column_name, $post_id ) {
     if ($column_name == 'abc') {
-        if ( has_post_thumbnail() ) {
-            $img_url = wp_get_attachment_image_src( get_post_thumbnail_id() );
-            echo '<img src="'. esc_url( $img_url[0] ).'" />';
-        } else { echo 'No Post Thumbnail Set'; }
+        echo "Ok":
     }
 }, 10, 2);
