1c2fad24d4e55e64c25ab29cf8f4cb6e15cfc32b
Himanshu Borah <borahhimanshu3@gmail.com>
2022-12-25 20:16:06 +0530

    Working - Update

--- inc/pdf-generator.php
+++ inc/pdf-generator.php
@@ -4,22 +4,15 @@ $post_type = 'booking';
 
 // Register the columns.
 add_filter( "manage_edit-booking_columns", function ( $defaults ) {
-	
-	$defaults['custom-one'] = 'Custom One';
-	$defaults['custom-two'] = 'Custom Two';
-
+	$defaults['custom-actions'] = 'Actions';
 	return $defaults;
 } );
 
 // Handle the value for each of the new columns.
 add_action( "manage_booking_posts_custom_column", function ( $column_name, $post_id ) {
 	
-	if ( $column_name == 'custom-one' ) {
+	if ( $column_name == 'custom-actions' ) {
 		echo 'Some value here';
-	}
-	
-	if ( $column_name == 'custom-two' ) {
-		echo "X";
 	}
 	
 }, 10, 2 );
