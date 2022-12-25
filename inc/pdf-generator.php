679bbb9966b82a0c1fd18edd8b809e24b50f0014
Himanshu Borah <borahhimanshu3@gmail.com>
2022-12-25 20:08:21 +0530

    ===

--- inc/pdf-generator.php
+++ inc/pdf-generator.php
@@ -1,16 +1,19 @@
 <?php
 
-add_filter('manage_edit-booking_columns', function($columns) {
+// Add the custom columns to the book post type:
+add_filter( 'manage_booking_columns', 'set_custom_edit_book_columns' );
+function set_custom_edit_book_columns($columns) {
     $columns['custom_actions'] = 'Actions';
     return $columns;
-});
+}
 
+// Add the data to the custom columns for the book post type:
+add_action( 'manage_booking_custom_column' , 'custom_book_column', 10, 2 );
+function custom_book_column( $column, $post_id ) {
+    switch ( $column ) {
+        case 'custom_actions' :
+            echo get_post_meta( $post_id , 'publisher' , true ); 
+            break;
 
-add_filter( 'manage_edit-booking_custom_column', function ( $output, $column_name, $id ) {
-
-	if( $column_name == 'custom_actions' ) { 
-		$output = $id;
-	}
-	return $output;
-}, 10, 3);
-
+    }
+}
