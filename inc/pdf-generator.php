2dc696626def8eb7f2fb9b4e02a4d8d74d6ec07e
Himanshu Borah <borahhimanshu3@gmail.com>
2022-12-26 14:44:47 +0530

    Updated

--- inc/pdf-generator.php
+++ inc/pdf-generator.php
@@ -12,14 +12,17 @@ add_filter( "manage_edit-booking_columns", function ( $defaults ) {
 add_action( "manage_booking_posts_custom_column", function ( $column_name, $post_id ) {
 	
 	if ( $column_name == 'custom-actions' ) {
-		$property_id  = get_post_meta( $post_id, 'rvr_property_id', true );
-		$perma = get_permalink($property_id);
+		$property_id      = get_post_meta( $post_id, 'rvr_property_id', true );
 
-		$obj = [
-			$perma
-		]
+		$total            = get_post_meta( $post_id, 'rvr_total_price_eur', true );
+		$nights           = get_post_meta( $post_id, 'rvr_staying_nights', true );
+		$nights_price     = get_post_meta( $post_id, 'rvr_price_per_night_eur', true );
+		$booking_username = get_post_meta( $post_id, 'rvr_renter_name', true );
+		$check_in_date    = get_post_meta( $post_id, 'rvr_check_in', true );
+		$URL              = get_permalink( $property_id );
+		
 		echo '<div style="padding-top:10px;">
-				<btn onclick="loadWindowFromURL(\'' . ...$obj . '\')" style="cursor: pointer !important; background-color: #2271b1; color: white; padding: 7px 10px; border-radius: 5px;">
+				<btn onclick="loadWindowFromURL(\'' . $URL . ', ' . $booking_username . ', ' . $check_in_date . ', ' . $nights . ',' . $nights_price . ', ' . $total . '\')" style="cursor: pointer !important; background-color: #2271b1; color: white; padding: 7px 10px; border-radius: 5px;">
 					Generate PDF
 				</btn>
 			  </div>';
