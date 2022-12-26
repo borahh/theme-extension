a953869d12aba5a5ed7d7c9cedc34bebb3e45d57
Himanshu Borah <borahhimanshu3@gmail.com>
2022-12-26 15:44:10 +0530

    Added Delay

--- assets/js/pdf-downloader.js
+++ assets/js/pdf-downloader.js
@@ -1,5 +1,9 @@
 window.addEventListener('load', function () {
-    if(document.getElementById('printingMode')) {
-     javascript:window.print();
+    if (document.getElementById('printingMode')) {
+        var delayInMilliseconds = 2000; //2 second
+        setTimeout(function() {
+            javascript:window.print();
+        }, delayInMilliseconds);
+     
     }
  })
