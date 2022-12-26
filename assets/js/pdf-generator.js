aece52305ba4abb956d906a5656bd06b1c9d15c5
Himanshu Borah <borahhimanshu3@gmail.com>
2022-12-26 12:39:01 +0530

    ===

--- assets/js/pdf-generator.js
+++ assets/js/pdf-generator.js
@@ -4,3 +4,24 @@ function fnprint() {
     document.getElementById("iframeid").contentWindow.print();
 
 }
+
+
+function loadWindowURL() {
+    let windowName = 'w_' + Date.now() + Math.floor(Math.random() * 100000).toString();
+var form = document.createElement("form");
+form.setAttribute("method", "post");
+form.setAttribute("action", "openData.do");
+
+form.setAttribute("target", windowName);
+
+var hiddenField = document.createElement("input"); 
+hiddenField.setAttribute("type", "hidden");
+hiddenField.setAttribute("name", "message");
+hiddenField.setAttribute("value", "val");
+form.appendChild(hiddenField);
+document.body.appendChild(form);
+
+window.open('', windowName);
+
+form.submit();
+}
