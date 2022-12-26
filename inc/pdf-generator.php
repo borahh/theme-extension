98269ef0fec75bbe3be2cbbf6d1b106251526d4f
Himanshu Borah <borahhimanshu3@gmail.com>
2022-12-26 12:49:28 +0530

    Perma Update

--- inc/pdf-generator.php
+++ inc/pdf-generator.php
@@ -12,20 +12,13 @@ add_filter( "manage_edit-booking_columns", function ( $defaults ) {
 add_action( "manage_booking_posts_custom_column", function ( $column_name, $post_id ) {
 	
 	if ( $column_name == 'custom-actions' ) {
+		$property_id  = get_post_meta( $post_id, 'rvr_property_id', true );
+		$perma = get_permalink($property_id);
 		echo '<div style="padding-top:10px;">
-				<btn onclick="loadWindowFromURL(\'' . get_permalink($post_id) . '\')" style="cursor: pointer !important; background-color: #2271b1; color: white; padding: 7px 10px; border-radius: 5px;">
+				<btn onclick="loadWindowFromURL(\'' . $perma . '\')" style="cursor: pointer !important; background-color: #2271b1; color: white; padding: 7px 10px; border-radius: 5px;">
 					Generate PDF
 				</btn>
 			  </div>';
 	}
-	// if ( $column_name == 'custom-actions' ) {
-	// 	echo '<div style="padding-top:10px;">
-	// 			<iframe style="visibility:hidden;" src="https://uniquevillastbarth.com/property/villa-ixfalia/" id="iframeid"></iframe>
-	// 			<btn onclick="fnprint()" style="cursor: pointer !important; background-color: #2271b1; color: white; padding: 7px 10px; border-radius: 5px;">
-	// 				Generate PDF
-	// 			</btn>
-	// 		  </div>';
-	// }
-
 	
 }, 10, 2 );
