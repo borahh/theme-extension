cbf2884d88d0cf5bb5ce0a6b55f1d9b3c19ac835
Himanshu Borah <borahhimanshu3@gmail.com>
2022-12-26 11:36:55 +0530

    ===

--- inc/pdf-generator.php
+++ inc/pdf-generator.php
@@ -12,9 +12,11 @@ add_filter( "manage_edit-booking_columns", function ( $defaults ) {
 add_action( "manage_booking_posts_custom_column", function ( $column_name, $post_id ) {
 	
 	if ( $column_name == 'custom-actions' ) {
-		echo '
-        <btn onclick="document.getElementById("print-iframe").contentWindow.print()" style="cursor: pointer !important; background-color: #2271b1; color: white; padding: 7px 10px; border-radius: 5px;">Generate PDF</btn>
-        </div>';
+		echo '<div style="padding-top:10px;">
+				<btn style="cursor: pointer !important; background-color: #2271b1; color: white; padding: 7px 10px; border-radius: 5px;">
+					Generate PDF
+				</btn>
+			  </div>';
 	}
 
 	
