4d3caa88cdd8797bad45872a3c6eab6794d0b2d8
Himanshu Borah <borahhimanshu3@gmail.com>
2022-12-30 15:58:14 +0530

    Assets

--- theme-extension.php
+++ theme-extension.php
@@ -44,10 +44,15 @@ function my_load_scripts($hook) {
 	// PDF content
 	$pdfContent_ver  = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'assets/js/pdf-downloader.js' ));
 
+	$showJs_ver = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'assets/js/show-hide.js' ));
+	$custom_css_ver = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'assets/css/custom.css' ));
 
 	// Run code only for Single post page
 	if ( is_single() && 'property' == get_post_type() ) {
 		wp_enqueue_script( 'pdfGenrator', plugins_url( 'assets/js/pdf-downloader.js', __FILE__ ), NULL, $pdfContent_ver );	
+		wp_enqueue_script( 'showHide', plugins_url( 'assets/js/show-hide.js', __FILE__ ), NULL, $showJs_ver );	
+		wp_enqueue_style( 'showHide', 	plugins_url( 'assets/css/custom.css', 	 __FILE__ ), false, $custom_css_ver );
+
 	}
 	wp_enqueue_script( 'calendar', plugins_url( 'assets/js/calendar.js', __FILE__ ), array(), $my_js_ver );
 	wp_enqueue_style( 'calendar', 	plugins_url( 'assets/css/calendar.css', 	 __FILE__ ), false, $my_css_ver );
