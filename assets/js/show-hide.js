79c3198869ce3bc13200d4e2dcaa8d4bad2e5dfe
Himanshu Borah <borahhimanshu3@gmail.com>
2022-12-30 16:26:05 +0530

    Fixed

--- assets/js/show-hide.js
+++ assets/js/show-hide.js
@@ -1,12 +1,8 @@
 document.getElementById('toggleElement').onclick = function() {
     var element = document.getElementById('contentToggle');
-    if (element.className === 'descHide') {
-      element.className = '';
-    //   document.getElementsByTagName('body')[0].className = 'on';
-    //   document.getElementById('contentToggle').className = 'active';
+    if (element.classList.contains("descHide")) {
+        element.classList.remove("descHide")
     } else {
-      element.className = 'descHide';
-    //   document.getElementsByTagName('body')[0].className = 'off';
-    //   document.getElementById('contentToggle').className = '';
+        element.classList.add("descHide")
     }
   }
