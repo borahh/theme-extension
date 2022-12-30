19159b0458faedbac14a662ac8582cc48e5f77ca
Himanshu Borah <borahhimanshu3@gmail.com>
2022-12-30 16:06:10 +0530

    ===

--- assets/js/show-hide.js
+++ assets/js/show-hide.js
@@ -1,12 +1,12 @@
-document.getElementById('show-element').onclick = function() {
-    var element = document.getElementById('to-show');
-    if (element.className === 'hide') {
-      element.className = 'show';
-      document.getElementsByTagName('body')[0].className = 'on';
-      document.getElementById('show-element').className = 'active';
+document.getElementById('toggleElement').onclick = function() {
+    var element = document.getElementById('contentToggle');
+    if (element.className === 'descHide') {
+      element.className = 'descShow';
+    //   document.getElementsByTagName('body')[0].className = 'on';
+    //   document.getElementById('contentToggle').className = 'active';
     } else {
-      element.className = 'hide';
-      document.getElementsByTagName('body')[0].className = 'off';
-      document.getElementById('show-element').className = '';
+      element.className = 'descHide';
+    //   document.getElementsByTagName('body')[0].className = 'off';
+    //   document.getElementById('contentToggle').className = '';
     }
   }
