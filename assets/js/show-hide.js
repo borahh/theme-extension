430ac7128b4d06c7c527c3e3d2be0be5bd071e09
Himanshu Borah <borahhimanshu3@gmail.com>
2022-12-30 16:33:11 +0530

    Height

--- assets/js/show-hide.js
+++ assets/js/show-hide.js
@@ -1,8 +1,31 @@
-document.getElementById('toggleElement').onclick = function() {
+function getAbsoluteHeight(el) {
+    // Get the DOM Node if you pass in a string
+    el = (typeof el === 'string') ? document.querySelector(el) : el; 
+  
+    var styles = window.getComputedStyle(el);
+    var margin = parseFloat(styles['marginTop']) +
+                 parseFloat(styles['marginBottom']);
+  
+    return Math.ceil(el.offsetHeight + margin);
+}
+  
+function borahh_get_height() {
+    var parent = document.getElementById('contentToggle');
+    var children = parent.querySelectorAll('p');
+    var slicedArray = children.slice(0, 3);
+    var height = 0;
+    slicedArray.forEach(element => {
+        height = getAbsoluteHeight(element) + height;
+    });
+
+    return height;
+}
+document.getElementById('toggleElement').onclick = function () {
     var element = document.getElementById('contentToggle');
     if (element.classList.contains("descHide")) {
         element.classList.remove("descHide")
     } else {
         element.classList.add("descHide")
     }
+    console.log(borahh_get_height());
   }
