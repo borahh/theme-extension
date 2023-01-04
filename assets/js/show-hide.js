42d9bbe2a239832d7e6f69a601132dbe7b1c5b48
Dinesh485 <ghantasaladinesh@gmail.com>
2023-01-04 16:30:23 +0530

    .

--- assets/js/show-hide.js
+++ assets/js/show-hide.js
@@ -46,7 +46,11 @@ document.getElementById('toggleElement').onclick = function () {
         const btn = acc.querySelector('.accordion-btn')
         const container = acc.querySelector('.accordion-content')
         btn.addEventListener('click', () =>{
-            console.log(container.style.height)
+            if(container.style.height){
+                container.style.height = container.scrollHeight + 'px'
+            }else{
+                container.style.height = null
+            }
         })
     })
   })
