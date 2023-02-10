b61f2059045dbeafa35cb5bff9e6b8cabf1410d6
Himanshu Borah <borahhimanshu3@gmail.com>
2023-02-10 10:50:08 +0530

    ...

--- assets/js/show-hide.js
+++ assets/js/show-hide.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', () =>{
     
    
-    let pricePerNight = document.querySelector('.price-per-night').value
-    const floatingBookWidget = `<div class = 'floating-booking-widget'> <div class = 'price'>From <br> <span>$${pricePerNight}</span></div><a class="rvr-booking-cta" href = "#rvr_booking_widget-1">Book now</a></div>`
+    let value = document.querySelector('.vrc_price').value
+    const floatingBookWidget = `<div class = 'floating-booking-widget'> <div class = 'price'>From <br> <span>$${value}</span></div><a class="rvr-booking-cta" href = "#rvr_booking_widget-1">Book now</a></div>`
     document.body.insertAdjacentHTML('beforeend',floatingBookWidget)
     const bookingform = document.querySelector('#rvr_booking_widget-1')
     const observer = new IntersectionObserver((entries) =>{
