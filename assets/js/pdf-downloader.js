d65f5738f88eb472f8b4499bb3264a83f45d2ed2
Himanshu Borah <borahhimanshu3@gmail.com>
2023-02-10 10:30:14 +0530

    ...

--- assets/js/pdf-downloader.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function () {
-    if (document.getElementById('printingMode')) {
-        var delayInMilliseconds = 2000; //2 second
-        setTimeout(function() {
-            javascript:window.print();
-        }, delayInMilliseconds);
-     
-    }
- })
