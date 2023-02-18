56eda38464fdffe8991880b5b1fb354dc6322677
Himanshu Borah <borahhimanshu3@gmail.com>
2023-02-18 19:35:16 +0530

    ...

--- assets/js/updateMetaFields.js
+++ assets/js/updateMetaFields.js
@@ -1,16 +1,19 @@
 jQuery(document).ready(function($) {
-  $('body').on('focus click', 'input', function (e) {
-     const id = $(e.target).attr('name');
-    if (!id) return;
-    const matches = id.match(/rvr_seasonal_plans\[(\d*)\]\[rvr_seasonal_plans_group\]\[(\d*)\]\[rvr_plan_week_price\]/);
+  // Get all input fields matching the pattern
+  const inputs = $('input[name^="rvr_seasonal_plans"][name$="[rvr_plan_week_price]"]');
+  // Loop through each input field
+  inputs.each(function() {
+    // Extract the numbers from the input field's "name" attribute
+    const matches = this.name.match(/rvr_seasonal_plans\[(\d*)\]\[rvr_seasonal_plans_group\]\[(\d*)\]\[rvr_plan_week_price\]/);
     if (!matches) return;
     const num1 = parseInt(matches[1]), num2 = parseInt(matches[2]);
     if (isNaN(num1) || isNaN(num2)) return;
+    // Find the corresponding "rvr_plan_night_price" input field
     const target = $(`[name="rvr_seasonal_plans\\[${num1}\\]\\[rvr_seasonal_plans_group\\]\\[${num2}\\]\\[rvr_plan_night_price\\]"]`);
     if (!target.length) return;
-    const val = parseInt(e.target.value);
-	const perDay = (val / 7);
-    console.log(perDay);
+    // Perform the calculation and update the "rvr_plan_night_price" input field
+    const val = parseInt(this.value);
+    const perDay = (val / 7);
     target.eq(0).val(isNaN(val) ? 0 : perDay.toFixed(7));
-  })
+  });
 });
