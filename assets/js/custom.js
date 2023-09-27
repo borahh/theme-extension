01ac28f59eb87f722b4715ae6ce9ed0f6a617247
Himanshu Borah <borahhimanshu3@gmail.com>
2023-09-27 19:47:45 +0530

    ...

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,3 +4,24 @@ document.querySelector(
 document.querySelector(
   ".rh_prop_search__selectwrap .bs-searchbox"
 ).style.display = "none";
+
+let currentStep = 1;
+
+document.addEventListener("DOMContentLoaded", function () {
+  showStep(currentStep);
+});
+
+function navigate(direction) {
+  currentStep += direction;
+  showStep(currentStep);
+}
+
+function showStep(step) {
+  let steps = document.querySelectorAll(".step");
+  steps.forEach((el, index) => {
+    el.classList.remove("active");
+    if (index + 1 === step) {
+      el.classList.add("active");
+    }
+  });
+}
